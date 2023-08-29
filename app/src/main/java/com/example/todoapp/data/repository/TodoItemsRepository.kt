472d9d34c5230b1a3c28c64a924948d8b6a6a9d0
nasty <nastyakras1707@gmail.com>
2023-08-29 08:27:20 +0300

    Refactor repository

--- app/src/main/java/com/example/todoapp/data/repository/TodoItemsRepository.kt
+++ app/src/main/java/com/example/todoapp/data/repository/TodoItemsRepository.kt
@@ -1,106 +1,75 @@
 package com.example.todoapp.data.repository
 
-import android.content.res.Resources.NotFoundException
-import android.database.sqlite.SQLiteException
-import com.example.todoapp.data.network.HardcodedTodoItemDataSource
+import androidx.annotation.MainThread
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.todoapp.data.db.LocalTodoItemDataSource
-import com.example.todoapp.presentation.fragments.todo_item.TodoItemUIState
 import com.example.todoapp.data.mappers.toTodoItem
 import com.example.todoapp.data.mappers.toTodoItemDto
 import com.example.todoapp.data.mappers.toTodoItemEntity
+import com.example.todoapp.data.network.HardcodedTodoItemDataSource
+import com.example.todoapp.presentation.fragments.todo_item.TodoItemUIState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import java.io.IOException
 
 class TodoItemsRepository(
     private val remoteDataSource: HardcodedTodoItemDataSource,
     private val localDataSource: LocalTodoItemDataSource
 ) {
+
+    private var _items = MutableLiveData<List<TodoItemUIState>>()
+    val items: LiveData<List<TodoItemUIState>>
+        get() = _items
+
+
+    @MainThread
+    suspend fun getItems() {
+        val list = withContext(Dispatchers.Default) {
+            returnFromDb()
+        }
+        _items.value = list
+        refreshItems()
+    }
+
     suspend fun refreshItems() =
         withContext(Dispatchers.Default) {
-            try {
-                val loadedList = remoteDataSource.getItems()
-                localDataSource.upsertAll(loadedList.map { it.toTodoItemEntity() })
-            } catch (e: Exception) {
-                throw Exception()
-            }
-        }
-
-    suspend fun getItems(): List<TodoItemUIState> =
-        withContext(Dispatchers.Default) {
-            try {
-                refreshItems()
-                returnFromDb()
-            } catch (_: IOException) {
-                returnFromDb()
-                throw IOException()
-            } catch (_: SQLiteException) {
-                throw SQLiteException()
-            }
+            val loadedList = remoteDataSource.getItems()
+            localDataSource.upsertAll(loadedList.map { it.toTodoItemEntity() })
+            _items.postValue(returnFromDb())
         }
 
     suspend fun getItemById(id: String) =
         withContext(Dispatchers.Default) {
-            try {
-                val item = remoteDataSource.getItemById(id)
-                item?.toTodoItem()
-            } catch (_: IOException) {
-                val item = localDataSource.getItemById(id) ?: throw NotFoundException()
-                item.toTodoItem()
-                throw IOException()
-            } catch (_: SQLiteException) {
-                throw SQLiteException()
-            }
+            localDataSource.getItemById(id)?.toTodoItem()
         }
 
 
     suspend fun addItem(item: TodoItemUIState) =
         withContext(Dispatchers.Default) {
-            try {
-                remoteDataSource.addItem(item.toTodoItemDto())
-                localDataSource.insert(item.toTodoItemEntity())
-            } catch (_: IOException) {
-                localDataSource.insert(item.toTodoItemEntity())
-                //добавить операцию в очередь до появления интернета
-            } catch (_: SQLiteException) {
-                throw SQLiteException()
-            }
+            val itemEntity = item.toTodoItemEntity()
+            localDataSource.insert(itemEntity)
+            _items.postValue(returnFromDb())
+            remoteDataSource.addItem(itemEntity.toTodoItemDto())
         }
 
     suspend fun updateItem(item: TodoItemUIState) =
         withContext(Dispatchers.Default) {
-            try {
-                remoteDataSource.updateItem(item.toTodoItemDto())
-                localDataSource.upsertItem(item.toTodoItemEntity())
-            } catch (_: IOException) {
-                localDataSource.upsertItem(item.toTodoItemEntity())
-                //добавить операцию в очередь до появления интернета
-            } catch (_: SQLiteException) {
-                throw SQLiteException()
-            }
+            val itemEntity = item.toTodoItemEntity()
+            localDataSource.upsertItem(itemEntity)
+            _items.postValue(returnFromDb())
+            remoteDataSource.updateItem(itemEntity.toTodoItemDto())
         }
 
 
     suspend fun deleteItem(id: String) =
         withContext(Dispatchers.Default) {
-            try {
-                remoteDataSource.deleteItem(id)
-                localDataSource.deleteItem(id)
-            } catch (_: IOException) {
-                localDataSource.deleteItem(id)
-                //добавить операцию в очередь до появления интернета
-            } catch (_: SQLiteException) {
-                throw SQLiteException()
-            }
+            localDataSource.deleteItem(id)
+            _items.postValue(returnFromDb())
+            remoteDataSource.deleteItem(id)
         }
 
     private suspend fun returnFromDb(): List<TodoItemUIState> =
         withContext(Dispatchers.Default) {
-            try {
-                val itemsFromDb = localDataSource.getItems().map { it.toTodoItem() }
-                itemsFromDb
-            } catch (_: SQLiteException) {
-                throw SQLiteException()
-            }
+            localDataSource.getItems().map { it.toTodoItem() }
         }
 }
